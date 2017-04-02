25ec6c08f64683d504f8040509bedc147a204dde
Dave Woodward <davidwoodwrd@hotmail.com>
2017-04-01 23:00:30 -0500

    Implement Unpublish

--- unpublish.js
+++ unpublish.js
@@ -1,5 +1,7 @@
 const config = require('./config');
 const log = require('./log');
+const fetch = require('./fetch');
+const chalk = require('chalk');
 
 exports.command = 'unpublish <name> [options]';
 
@@ -15,7 +17,33 @@ exports.builder = function(yargs) {
 
 exports.handler = function(argv) {
   log.setCurrentLevel(argv.verbose ? log.LOG_LEVEL.VERBOSE : argv.quiet ? log.LOG_LEVEL.SILENT : log.LOG_LEVEL.NORMAL);
-  log.write('UNPUBLISH CALLED: SILENT', log.LOG_LEVEL.SILENT);
-  log.write('UNPUBLISH CALLED: NORMAL', log.LOG_LEVEL.NORMAL);
-  log.write('UNPUBLISH CALLED: VERBOSE', log.LOG_LEVEL.VERBOSE);
+  log.writeLine(chalk.white('==============================================================================='), log.LOG_LEVEL.VERBOSE);
+  log.writeLine(chalk.white(`= Unublishing ${argv.name}`), log.LOG_LEVEL.VERBOSE);
+  log.writeLine(chalk.white('==============================================================================='), log.LOG_LEVEL.VERBOSE);
+  let finished = () => {
+    log.writeLine(chalk.white('==============================================================================='), log.LOG_LEVEL.VERBOSE);
+    log.writeLine(chalk.white(`= Finished Unpublishing ${argv.name}`), log.LOG_LEVEL.VERBOSE);
+    log.writeLine(chalk.white('==============================================================================='), log.LOG_LEVEL.VERBOSE);
+  };
+
+  let url = `${config.API_URL}unpublish/${argv.name}`;
+  log.write(chalk.white(`Removing ${argv.name} via ${url}...`), log.LOG_LEVEL.VERBOSE);
+  return fetch(url, {
+      method: 'DELETE',
+      headers: {
+          'Authorization': `token ${argv.key}`
+      }
+  }).then((res) => {
+    log.writeLine(chalk.green('COMPLETE'), log.LOG_LEVEL.VERBOSE);
+    if (res.status === 200 && res.statusText === 'OK') {
+      log.writeLine(chalk.green(`The application ${argv.name} was successfully removed from ${config.APP_URL_TEMPLATE.replace('APP_NAME', argv.name)}`), log.LOG_LEVEL.NORMAL);
+    } else {
+      log.writeLine(chalk.red(`An unexpected response was received from the server: ${res.statusText}`), log.LOG_LEVEL.ERROR);
+    }
+    finished();
+  }).catch((error) => {
+    let message = error.response && error.response.statusText ? error.response.statusText : error;
+    log.writeLine(chalk.red(`Unable to remove the application ${argv.name}: ${message}`), log.LOG_LEVEL.ERROR);
+    finished();
+  });
 };
