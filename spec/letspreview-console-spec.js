ae6ab4da280bab47be46d9ab3f4523a7ad433ec7
Dave Woodward <davidwoodwrd@hotmail.com>
2017-05-19 14:37:42 -0500

    fix(console): Fix required module paths and write related tests.

---
+++ spec/letspreview-console-spec.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const execFile = require('child_process').execFile;
+const config = require('../lib/core/config');
+
+describe('letspreview', () => {
+  it('should return the full help message when the user fails to pass a command to execute.', (done) => {
+    execFile('node', ['index'], (error, stdout, stderr) => {
+      expect(stdout).toBeFalsy();
+      expect(stderr.replace(/\s|\r?\n|\r/g, '')).toEqual('index<cmd><name>[options]Commands:publish<name>[options]Pushestheapplicationtoletspreview.io.unpublish<name>[options]Removestheapplicationfromletspreview.io.GlobalOptions-v,--verboseDisplayDebuggingOutput[boolean]-h,--helpShowHelp[boolean]-q,--quietSuppressOutput[boolean]Youmustspecifyacommandtoexecute.');
+      done();
+    });
+  });
+});
+
+describe('letspreview publish', () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+  it('should return a missing argument error message when the user neglects to provide an application name.', (done) => {
+    execFile('node', ['index', 'publish'], (error, stdout, stderr) => {
+      expect(stdout).toBeFalsy();
+      expect(stderr).toMatch(/Not enough non-option arguments: got 0, need at least 1/);
+      done();
+    });
+  });
+
+  it('should return a missing key error message when the user neglects to provide an api key.', (done) => {
+    execFile('node', ['index', 'publish', 'hello'], (error, stdout, stderr) => {
+      expect(stdout).toBeFalsy();
+      expect(stderr).toMatch(/Missing required argument: key/);
+      done();
+    });
+  });
+
+  it('should return an unauthorized error message when the user passes an invalid api key.', (done) => {
+    execFile('node', ['index', 'publish', 'hello', '-k', 'invalid-key'], (error, stdout, stderr) => {
+      expect(stderr).toBeFalsy();
+      expect(stdout).toMatch(/Unauthorized/);
+      done();
+    });
+  });
+});
+
+describe('letspreview unpublish', () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+  it('should return a missing argument error message when the user neglects to provide an application name.', (done) => { // eslint-disable-line jasmine/no-spec-dupes
+    execFile('node', ['index', 'unpublish', '-k', config.API_KEY], (error, stdout, stderr) => {
+      expect(stdout).toBeFalsy();
+      expect(stderr).toMatch(/Not enough non-option arguments: got 0, need at least 1/);
+      done();
+    });
+  });
+
+  it('should return a missing argument error message when an invalid application name is passed.', (done) => { // eslint-disable-line jasmine/no-spec-dupes
+    execFile('node', ['index', 'unpublish', 'invalid-name', '-k', config.API_KEY], (error, stdout, stderr) => {
+      expect(stderr).toBeFalsy();
+      expect(stdout).toMatch(/Unable to remove the application invalid-name: Not Found/);
+      done();
+    });
+  });
+
+  it('should return a missing key error message when the user neglects to provide an api key.', (done) => { // eslint-disable-line jasmine/no-spec-dupes
+    execFile('node', ['index', 'unpublish', 'hello'], (error, stdout, stderr) => {
+      expect(stdout).toBeFalsy();
+      expect(stderr).toMatch(/Missing required argument: key/);
+      done();
+    });
+  });
+
+  it('should return an unauthorized error message when the user passes an invalid api key.', (done) => { // eslint-disable-line jasmine/no-spec-dupes
+    execFile('node', ['index', 'unpublish', 'hello', '-k', 'invalid-key'], (error, stdout, stderr) => {
+      expect(stderr).toBeFalsy();
+      expect(stdout).toMatch(/Unauthorized/);
+      done();
+    });
+  });
+});
