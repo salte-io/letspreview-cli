78b0d3ca048fcc5ef1ef16f77c4d15621e93d162
Nick Woodward <nick@salte.io>
2017-05-25 15:09:13 -0500

    chore: dropped support for node < 6
    - Added an issue template
    - Made the binary check the node
      version prior to running the cli
    - Added license to each file

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-#!/usr/bin/env node
-
-'use strict';
+/**
+ * @license
+ * Copyright (c) 2017 The Let's Preview Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://letspreview.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://letspreview.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://letspreview.github.io/CONTRIBUTORS.txt
+ * Code distributed by Salte as part of the letspreview project is also
+ * subject to an additional IP rights grant found at
+ * http://letspreview.github.io/PATENTS.txt
+ */
 
 require('yargs').usage('$0 <cmd> <name> [options]')
   .group(['v', 'h', 'q'], 'Global Options')
